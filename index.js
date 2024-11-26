95b7566090e58680388e28c1345cc54e041b3172
EhsanEIK <ehsankhan1997.ek@gmail.com>
2024-11-26 09:10:09 +0600

    updated cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 const port = process.env.PORT | 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000/'
+    ]
+}));
 app.use(express.json());
 
 
