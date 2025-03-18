e9f5f3c05b0621fcad58968541e175221e543e67
EhsanEIK <ehsankhan1997.ek@gmail.com>
2025-03-18 09:58:30 +0600

    updated versel.json file and deployed

--- index.js
+++ index.js
@@ -6,19 +6,8 @@ require('dotenv').config();
 const port = process.env.PORT | 5000;
 
 // middleware
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
-
-// app.use(cors({
-//     origin: [
-//         'http://localhost:3000/'
-//     ]
-// }));
-// app.use((req, res, next) => {
-//     res.header({ "Access-Control-Allow-Origin": "*" });
-//     next();
-// })
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
